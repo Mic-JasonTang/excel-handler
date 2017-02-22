0881bda8d14359640c0c58fc42bf2cbbf6819b72
yang.tang <yang.tang@chinaredstar.com>
2017-02-23 00:07:13 +0800

    use the css and js beautify the page

--- src/main/java/com/iclass/ExcelHandlerApplication.java
+++ src/main/java/com/iclass/ExcelHandlerApplication.java
@@ -4,11 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @EnableAutoConfiguration
 @MapperScan("com.iclass.excel.mybatis.mapper")
-@ImportResource(locations = "classpath:spring/*.xml")
 @SpringBootApplication
 public class ExcelHandlerApplication {
 
--- src/main/resources/static/js/fileupload.js
+++ src/main/resources/static/js/fileupload.js
@@ -1,16 +1,27 @@
 /**
  * Created by yang.tang on 2017/2/22.
  */
+$(function () {
+    var tips = $(".fileerrorTip");
+    tips.addClass("error");
+    $(".a-upload").on("change","input[type='file']",function(){
+        var filePath=$(this).val();
+        if(filePath.indexOf("xls")!=-1 || filePath.indexOf("xlsx")!=-1){
+            tips.html("").hide();
+            var arr=filePath.split('\\');
+            var fileName=arr[arr.length-1];
+            $(".showFileName").html(fileName);
+            tips.removeClass("error");
+        }else{
+            $(".showFileName").html("");
+            tips.html("您未上传文件，或者您上传文件类型有误！").show();
+            return false;
+        }
+    });
+    $("#uploadButton").on("click",function(){
+        if(!$(".fileerrorTip").hasClass("error")) {
+            $("#fileuploadfrom").submit();
+        }
+    })
+})
 
-function selectFile(fnUpload) {
-    var filename = fnUpload.value;
-    var mime = filename.toLowerCase().substr(filename.lastIndexOf("."));
-    if (mime == ".xls" || mime == ".xlsx") {
-        // alert(mime);
-    } else {
-        // alert(mime != ".xls");
-        // alert(mime != ".xlsx");
-        alert("请上传正确的excel文件(xls和xlsx文件)");
-        fnUpload.outerHTML = fnUpload.outerHTML;
-    }
-}
